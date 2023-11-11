8360c2212653a31c9632df915ff8651261a949ab
OlaniyiHope <olaniyihoppee@gmail.com>
2023-11-11 17:29:23 +0100

    all

--- src/fake-db/db/auth.js
+++ src/fake-db/db/auth.js
@@ -1,74 +1,28 @@
-import jwt from 'jsonwebtoken';
-import Mock from '../mock';
+import Mock from "../mock";
 
-const JWT_SECRET = 'jwt_secret_key';
-const JWT_VALIDITY = '7 days';
-
-const userList = [
-  {
-    id: 1,
-    role: 'SA',
-    name: 'HLHS',
-    username: 'HLHS',
-    email: 'hlhs@gmail.com',
-    avatar: '',
-    age: 25,
-  },
-];
+const JWT_SECRET = "jwt_secret_key";
+const JWT_VALIDITY = "7 days";
 
 // FOLLOWING CODES ARE MOCK SERVER IMPLEMENTATION
 // YOU NEED TO BUILD YOUR OWN SERVER
 // IF YOU NEED HELP ABOUT SERVER SIDE IMPLEMENTATION
 // CONTACT US AT support@ui-lib.com
 
-Mock.onPost('/api/auth/login').reply(async (config) => {
-  try {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    const { email } = JSON.parse(config.data);
-    const user = userList.find((u) => u.email === email);
-
-    if (!user) {
-      return [400, { message: 'Invalid email or password' }];
-    }
-    const accessToken = jwt.sign({ userId: user.id }, JWT_SECRET, {
-      expiresIn: JWT_VALIDITY,
-    });
-
-    return [
-      200,
-      {
-        accessToken,
-        user: {
-          id: user.id,
-          avatar: user.avatar,
-          email: user.email,
-          name: user.name,
-          role: user.role,
-        },
-      },
-    ];
-  } catch (err) {
-    console.error(err);
-    return [500, { message: 'Internal server error' }];
-  }
-});
-
-Mock.onPost('/api/auth/register').reply((config) => {
+Mock.onPost("/api/auth/register").reply((config) => {
   try {
     const { email, username } = JSON.parse(config.data);
     const user = userList.find((u) => u.email === email);
 
     if (user) {
-      return [400, { message: 'User already exists!' }];
+      return [400, { message: "User already exists!" }];
     }
     const newUser = {
       id: 2,
-      role: 'GUEST',
-      name: '',
+      role: "GUEST",
+      name: "",
       username: username,
       email: email,
-      avatar: '/assets/images/face-6.jpg',
+      avatar: "/assets/images/face-6.jpg",
       age: 25,
     };
     userList.push(newUser);
@@ -93,23 +47,23 @@ Mock.onPost('/api/auth/register').reply((config) => {
     ];
   } catch (err) {
     console.error(err);
-    return [500, { message: 'Internal server error' }];
+    return [500, { message: "Internal server error" }];
   }
 });
 
-Mock.onGet('/api/auth/profile').reply((config) => {
+Mock.onGet("/api/auth/profile").reply((config) => {
   try {
     const { Authorization } = config.headers;
     if (!Authorization) {
-      return [401, { message: 'Invalid Authorization token' }];
+      return [401, { message: "Invalid Authorization token" }];
     }
 
-    const accessToken = Authorization.split(' ')[1];
+    const accessToken = Authorization.split(" ")[1];
     const { userId } = jwt.verify(accessToken, JWT_SECRET);
     const user = userList.find((u) => u.id === userId);
 
     if (!user) {
-      return [401, { message: 'Invalid authorization token' }];
+      return [401, { message: "Invalid authorization token" }];
     }
 
     return [
@@ -126,6 +80,6 @@ Mock.onGet('/api/auth/profile').reply((config) => {
     ];
   } catch (err) {
     console.error(err);
-    return [500, { message: 'Internal server error' }];
+    return [500, { message: "Internal server error" }];
   }
 });
