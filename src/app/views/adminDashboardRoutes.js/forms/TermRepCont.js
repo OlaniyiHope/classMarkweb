459d836e79b6141e9c0e14aaa5b195f3f80cbacb
OlaniyiHope <olaniyihoppee@gmail.com>
2023-11-22 02:38:43 +0100

    all

--- src/app/views/adminDashboardRoutes.js/forms/TermRepCont.js
+++ src/app/views/adminDashboardRoutes.js/forms/TermRepCont.js
@@ -3,12 +3,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import TermRep from "./TermRep";
-import useAuth from "app/hooks/useAuth";
 
 const TermRepCont = () => {
-  // const { id } = useParams();
-  const { user } = useAuth();
-  const { id } = user; // Assuming _id is the correct field
+  const { id } = useParams();
 
   return (
     <div>
