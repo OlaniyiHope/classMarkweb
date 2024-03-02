a9d5a4ff348a35abdb620821f866e9e707aa3098
OlaniyiHope <olaniyihoppee@gmail.com>
2024-03-02 12:51:44 +0100

    all

--- src/app/components/Brand.jsx
+++ src/app/components/Brand.jsx
@@ -21,11 +21,45 @@ const Brand = ({ children }) => {
   const leftSidebar = settings.layout1Settings.leftSidebar;
   const { mode } = leftSidebar;
 
+  const [accountSettings, setAccountSettings] = useState({
+    name: "",
+    motto: "",
+    address: "",
+    phone: "",
+    phonetwo: "",
+    email: "",
+    sessionStart: "",
+    sessionEnd: "",
+    schoolLogo: "",
+  });
+  const apiUrl = process.env.REACT_APP_API_URL.trim();
+  useEffect(() => {
+    const fetchAccountSettings = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/api/account-setting`);
+        const { data } = response.data;
+
+        // Set the retrieved school settings to the state
+        setAccountSettings(data);
+      } catch (error) {
+        console.error("Error fetching school settings:", error);
+      }
+    };
+
+    fetchAccountSettings();
+  }, [apiUrl]);
   return (
     <BrandRoot>
       <Box display="flex" alignItems="center">
         <StyledSpan mode={mode} className="sidenavHoverShow">
-          <img src={logo} style={{ height: "100px" }} />
+          <img
+            src={`https://edupros.s3.amazonaws.com/${accountSettings.schoolLogo}`}
+            style={{
+              width: "200px",
+              height: "130px",
+              borderRadius: "50%",
+            }}
+          />
         </StyledSpan>
       </Box>
 
