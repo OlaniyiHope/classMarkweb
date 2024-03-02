8f221c8b2484baf93da4460baf853e488f300ddf
OlaniyiHope <olaniyihoppee@gmail.com>
2024-03-02 10:30:13 +0100

    all

--- src/app/views/adminDashboardRoutes.js/forms/Info8.js
+++ src/app/views/adminDashboardRoutes.js/forms/Info8.js
@@ -228,6 +228,7 @@ const Info8 = () => {
             <div class="col-xl-12 wow fadeInUp" data-wow-delay="1.5s">
               <div class="table-responsive full-data">
                 <table
+                  style={{ overflowX: "auto", maxWidth: "100%" }}
                   class="table-responsive-lg table display dataTablesCard student-tab dataTable no-footer"
                   id="example-student"
                 >
