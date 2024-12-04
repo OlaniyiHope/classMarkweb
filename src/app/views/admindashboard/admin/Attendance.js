c7142eadc9eb190b2fafdfc595b2be9490518579
olaniyihope2 <olaniyihope2@gmail.com>
2024-12-04 14:38:06 +0100

    all

--- src/app/views/admindashboard/admin/Attendance.js
+++ src/app/views/admindashboard/admin/Attendance.js
@@ -65,112 +65,28 @@ const Attendance = () => {
 
   const apiUrl = process.env.REACT_APP_API_URL;
 
-  const fetchStudentData = async (examId) => {
+  const fetchStudentData = async () => {
     try {
       const token = localStorage.getItem("jwtToken");
-      const headers = new Headers();
-      headers.append("Authorization", `Bearer ${token}`);
-
       const response = await fetch(
-        `${apiUrl}/api/get-all-attendance/${examId}`,
+        `${apiUrl}/api/students/${currentSession._id}/${selectedClass}`,
         {
-          headers,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
       );
 
       if (!response.ok) {
-        console.error(
-          "Failed to fetch student data. Response details:",
-          response
-        );
         throw new Error("Failed to fetch student data");
       }
 
       const data = await response.json();
-      return data;
+      setStudentData(data);
     } catch (error) {
       console.error("Error fetching student data:", error);
-      return { scores: [] }; // Return empty array if there's an error
     }
   };
-
-  // const handleManagePsyClick = async () => {
-  //   try {
-  //     const token = localStorage.getItem("jwtToken");
-  //     const headers = new Headers();
-  //     headers.append("Authorization", `Bearer ${token}`);
-
-  //     const response = await fetch(
-  //       `${apiUrl}/api/students/${currentSession._id}/${selectedClass}`,
-  //       {
-  //         headers,
-  //       }
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error("Failed to fetch student data");
-  //     }
-
-  //     const students = await response.json();
-
-  //     // Handle the case where no students are found
-  //     if (students.length === 0) {
-  //       console.warn("No students found for the selected class.");
-  //       // Proceed with the logic for initializing the state, etc.
-  //       // You might want to show a message to the user or take appropriate action.
-  //     } else {
-  //       // Assuming you want to pick the first student for now
-  //       const firstStudentId = students[0]._id;
-  //       setSelectedStudentId(firstStudentId);
-
-  //       const existingData = await fetchStudentData(selectedExam);
-
-  //       console.log("Response from fetchStudentData:", existingData);
-  //       console.log("Existing scores:", existingData.scores);
-
-  //       // Ensure that the scores are properly set in the initial state
-  //       const initialState = students.map((student) => {
-  //         const studentScore = existingData.scores.find(
-  //           (score) => score.studentId && score.studentId._id === student._id
-  //         );
-
-  //         console.log(`Student ${student._id} - Existing Score:`, studentScore);
-
-  //         // const defaultTestScore = studentScore
-  //         //   ? studentScore.instruction !== undefined
-  //         //     ? studentScore.instruction
-  //         //     : 0
-  //         //   : 0;
-
-  //         // const defaultExamScore = studentScore
-  //         //   ? studentScore.independently !== undefined
-  //         //     ? studentScore.independently
-  //         //     : 0
-  //         //   : 0;
-
-  //         return {
-  //           studentId: student._id,
-  //           studentName: student.studentName,
-  //           AdmNo: student.AdmNo,
-  //           instruction: studentScore ? studentScore.instruction || 0 : 0,
-  //           independently: studentScore ? studentScore.independently || 0 : 0,
-  //           punctuality: studentScore ? studentScore.punctuality || 0 : 0,
-  //           talking: studentScore ? studentScore.talking || 0 : 0,
-  //           eyecontact: studentScore ? studentScore.eyecontact || 0 : 0,
-  //           remarks: studentScore ? studentScore.remarks || "" : "",
-  //           premarks: studentScore ? studentScore.premarks || "" : "",
-  //         };
-  //       });
-
-  //       console.log("Initial state:", initialState);
-
-  //       setStudentData(initialState);
-  //       setShowMarkManagement(true);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching student data:", error);
-  //   }
-  // };
 
   const handleClassChange = (event) => {
     const newSelectedClass = event.target.value;
@@ -186,133 +102,6 @@ const Attendance = () => {
     return selectedExam ? selectedExam.name : "";
   };
 
-  // const handleSaveChanges = async () => {
-  //   try {
-  //     const marks = studentData.map((student) => {
-  //       const {
-  //         studentId,
-  //         instruction = 5,
-  //         independently = 5,
-  //         punctuality = 5,
-  //         talking = 5,
-  //         eyecontact = 5,
-  //         remarks = 5,
-  //         premarks = 5,
-  //       } = student;
-
-  //       return {
-  //         studentId,
-
-  //         instruction,
-  //         independently,
-  //         punctuality,
-  //         talking,
-  //         eyecontact,
-  //         remarks,
-  //         premarks,
-  //       };
-  //     });
-
-  //     console.log("Updated Marks:", marks);
-
-  //     const token = localStorage.getItem("jwtToken");
-  //     const headers = new Headers();
-  //     headers.append("Authorization", `Bearer ${token}`);
-
-  //     // Check if there are existing marks by verifying the examId and subjectId
-  //     if (selectedExam) {
-  //       const responseCheckMarks = await fetch(
-  //         `${apiUrl}/api/get-all-psy/${selectedExam}`,
-  //         {
-  //           headers,
-  //         }
-  //       );
-
-  //       console.log("Response from Check Marks:", responseCheckMarks);
-
-  //       if (responseCheckMarks.ok) {
-  //         const responseData = await responseCheckMarks.json();
-  //         const existingMarks = responseData.scores || [];
-
-  //         // Check if there are existing marks
-  //         if (existingMarks.length > 0) {
-  //           // Existing marks found, proceed with updating
-  //           const responseUpdateMarks = await fetch(
-  //             `${apiUrl}/api/update-all-psy`,
-  //             {
-  //               method: "PUT",
-  //               headers: {
-  //                 ...headers,
-  //                 "Content-Type": "application/json",
-  //               },
-  //               body: JSON.stringify({
-  //                 examId: selectedExam,
-
-  //                 updates: marks,
-  //               }),
-  //             }
-  //           );
-
-  //           console.log("Request Payload:", {
-  //             examId: selectedExam,
-
-  //             updates: marks,
-  //           });
-
-  //           console.log("Response from Update Marks:", responseUpdateMarks);
-
-  //           if (!responseUpdateMarks.ok) {
-  //             const errorMessage = await responseUpdateMarks.text();
-  //             console.error(
-  //               `Failed to update marks. Server response: ${errorMessage}`
-  //             );
-  //             throw new Error("Failed to update marks");
-  //           } else {
-  //             // Notify success using toast
-  //             toast.success("Marks updated successfully!");
-  //           }
-  //         } else {
-  //           // No existing marks found, proceed to create new marks
-  //           const responseSaveMarks = await fetch(`${apiUrl}/api/save-psy`, {
-  //             method: "POST",
-  //             headers: {
-  //               ...headers,
-  //               "Content-Type": "application/json",
-  //             },
-  //             body: JSON.stringify({
-  //               examId: selectedExam,
-
-  //               updates: marks,
-  //             }),
-  //           });
-
-  //           console.log("Response from Save Marks:", responseSaveMarks);
-
-  //           if (!responseSaveMarks.ok) {
-  //             const errorMessage = await responseSaveMarks.text();
-
-  //             console.error(
-  //               `Failed to save marks. Server response: ${errorMessage}`
-  //             );
-  //             throw new Error("Failed to save marks");
-  //           } else {
-  //             // Notify success using toast
-  //             toast.success("Marks saved successfully!");
-  //           }
-  //         }
-  //       } else {
-  //         // Handle other response statuses
-  //         // ...
-  //       }
-  //     }
-  //     // ... (remaining code)
-  //   } catch (error) {
-  //     console.error("Error saving marks:", error);
-  //     // ... (error handling)
-  //   }
-  // };
-
-  // Handles attendance status change
   const handleAttendanceChange = (index, status) => {
     const updatedStudents = [...studentData];
     updatedStudents[index].status = status;
@@ -321,8 +110,8 @@ const Attendance = () => {
 
   // Simulates fetching data and showing the table
   const handleManageAttendanceClick = () => {
-    // Fetch student data based on selected exam, class, and date
-    // Example: setStudentData(response.data);
+    fetchStudentData();
+
     setShowAttendance(true);
   };
 
@@ -429,7 +218,7 @@ const Attendance = () => {
                         <tr key={index}>
                           <td>{index + 1}</td>
                           <td>{student.AdmNo}</td>
-                          <td>{student.studentName}</td>
+                          <td>{student?.studentName}</td>
                           <td>
                             <TextField
                               select
